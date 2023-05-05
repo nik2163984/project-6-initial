f97e12896701bba0114ecb1488b97335a1acb718
github-nik2163984 <github-user-nik2163984@maricopa.edu>
2023-05-05 10:12:52 -0700

    Project 6

--- aggregator/src/main/java/com/example/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/com/example/aggregator/AggregatorApplication.java
@@ -3,6 +3,7 @@ package com.example.aggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AggregatorApplication {
 
--- dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
+++ dictionary/src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -8,6 +8,13 @@ public class DictionaryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DictionaryApplication.class, args);
+        /*ConfigurableApplicationContext cxt = SpringApplication.run(DictionaryApplication.class, args);
+        String[] beans = cxt.getBeanDefinitionNames();
+        AtomicInteger counter = new AtomicInteger(1);
+        Arrays.stream(beans)
+                .sorted()
+                .map(name -> counter.getAndIncrement() + ": " + name)
+                .forEach(System.out::println);*/
     }
 
 }
